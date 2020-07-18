cdaa62115129a147f9f80b9f090b425b9b26fe77
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-18 22:15:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,11 @@ def speak_to_grandma(phrase)
 
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "I Love YOU GRANDMA".downcase
+    elsif phrase == 'Hi Nana, how are you?'.downcase
   		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
+    elsif phrase == 'Hi!'.upcase
       return "NO, NOT SINCE 1938!"
     # NO, NOT SINCE 1938!
     else
